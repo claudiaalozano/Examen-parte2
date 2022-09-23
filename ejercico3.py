41e94a3bd11cad4fa92c5731d677d09d1f832ce3
claudiaalozano <clau.lozano.perez@gmail.com>
2022-09-23 16:48:52 +0200

    cscnscjknsk

---
+++ ejercico3.py
@@ -0,0 +1,16 @@
+
+def crear_listas(x):
+    x = range(n1, n2)
+    list(x)
+    print(list(x))
+
+n1= int(input("Introduce el numero:"))
+n2 = int(input("Introduce el numero: "))
+print(crear_listas())
+
+#x=range(0,11)
+
+#list(x)
+#print(list(x))
+
+
