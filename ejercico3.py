8abfe858c70d9dfa9d604d3d3540a235f2b27873
claudiaalozano <clau.lozano.perez@gmail.com>
2022-09-23 17:12:16 +0200

    Update ejercico3.py

--- ejercico3.py
+++ ejercico3.py
@@ -1,16 +1,31 @@
 
 def crear_listas(x):
-    x = range(n1, n2)
+    
     list(x)
     print(list(x))
+    
+
+def multiplos_de_5(x):
+    e1 = 0
+    e2 = 50
+    multiple = 5
+    list = []
+    i=0
+    for i in range(0, 101):
+        if multiple(i,5):
+            list.append(i)
+    return True if e1 and e2 % multiple == 0 else False
+        
+    
 
 n1= int(input("Introduce el numero:"))
 n2 = int(input("Introduce el numero: "))
-print(crear_listas())
 
-#x=range(0,11)
+x = range(n1, n2+1)
 
-#list(x)
-#print(list(x))
+print(crear_listas(x))
+print(multiplos_de_5(x))
+
+
 
 
