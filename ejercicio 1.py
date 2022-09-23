833336ff89a6e1b44394290b780435b72f00ab5c
claudiaalozano <clau.lozano.perez@gmail.com>
2022-09-23 20:24:23 +0200

    ejercicio 1 acabado.

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -11,12 +11,24 @@ matriz = [
 
 ]
 
-def suma():
-    i=0
-    for i in range :
-        elementos= sum(matriz)
-        if elementos == [3]:
-            print("Resultado correcto.", "El resultado es ", elementos)
-        else:
-            print("Resultado incorrecto.", "El resultado es ", elementos)
+#def suma():
+    #i=0
+    #for i in range :
+        #elementos= sum(matriz)
 
+def sum1(n):
+    sumita= v1 + v2 + v3 
+    if sumita == v4:
+        print("El resultado es correcto, los tres primeros elementos sumados son igual al cuarto.")
+    else:
+        print("La suma de los tres primeros elementos no es igual al cuarto.")
+
+
+
+n= int(input("Introduce el valor de la fila que quieres analizar de la matriz (0-3): "))
+v1 = matriz[n][0]
+v2 = matriz[n][1]
+v3 = matriz[n][2]
+v4 = matriz[n][3]
+
+print(sum1(n))
